ae7b81997b4d6bfed314e063547fc5db28eb2935
allansomensi <allansomensidev@proton.me>
2024-09-13 13:23:24 -0300

    refactor: remove `TonerColor` field from `toner` model
    The color of the toner is usually included in the model name, making the `Color` field unnecessary.

--- migrations/0001_create_toner_table.sql
+++ migrations/0001_create_toner_table.sql
@@ -1,7 +1,4 @@
-CREATE TYPE toner_color AS ENUM ('black', 'cyan', 'yellow', 'magenta');
-
 CREATE TABLE toners (
     id UUID PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    color toner_color NOT NULL
+    name VARCHAR(50) NOT NULL
 );
--- src/models/supplies/toner.rs
+++ src/models/supplies/toner.rs
@@ -1,46 +1,18 @@
-use std::str::FromStr;
-
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use uuid::Uuid;
-
-#[derive(Deserialize, Serialize, sqlx::Type)]
-#[sqlx(type_name = "toner_color", rename_all = "lowercase")]
-#[serde(rename_all = "snake_case")]
-pub enum TonerColor {
-    Black,
-    Cyan,
-    Yellow,
-    Magenta,
-}
-
-impl FromStr for TonerColor {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "black" => Ok(TonerColor::Black),
-            "cyan" => Ok(TonerColor::Cyan),
-            "yellow" => Ok(TonerColor::Yellow),
-            "magenta" => Ok(TonerColor::Magenta),
-            _ => Err(format!("Cor inválida! Erro: {}", s)),
-        }
-    }
-}
 
 #[derive(Deserialize, Serialize, FromRow)]
 pub struct Toner {
     pub id: Uuid,
     pub name: String,
-    pub color: TonerColor,
 }
 
 impl Toner {
-    pub fn new(name: &str, color: TonerColor) -> Self {
+    pub fn new(name: &str) -> Self {
         Toner {
             id: Uuid::new_v4(),
             name: String::from(name),
-            color,
         }
     }
 }
@@ -48,14 +20,12 @@ impl Toner {
 #[derive(Deserialize, Serialize)]
 pub struct CreateTonerRequest {
     pub name: String,
-    pub color: TonerColor,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct UpdateTonerRequest {
     pub id: Uuid,
     pub name: String,
-    pub color: TonerColor,
 }
 
 #[derive(Deserialize, Serialize)]
