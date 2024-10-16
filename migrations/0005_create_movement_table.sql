b1845cc37cca0f8fccc4104b1932a8d97c07446f
allansomensi <allansomensidev@proton.me>
2024-10-16 15:57:14 -0300

    refactor: simplify `movement` model and creation logic
    - Replaced `toner_id` and `drum_id` fields with a single generic `item_id`.
    - Added check in `create_movement` query to determine if the item is a toner or a drum.
    - Update migrations for the `movement` table.

--- migrations/0005_create_movement_table.sql
+++ migrations/0005_create_movement_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE movements (
     id UUID PRIMARY KEY,
     printer_id UUID REFERENCES printers(id) ON DELETE SET NULL,
-    toner_id UUID REFERENCES toners(id) ON DELETE SET NULL,
-    drum_id UUID REFERENCES drums(id) ON DELETE SET NULL,
+    item_id UUID NOT NULL,
     quantity INTEGER NOT NULL,
-    created_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
