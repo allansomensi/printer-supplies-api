9ec6dc4465e80549c2f60f7180c3cb231dd61523
Allan Somensi <allansomensidev@gmail.com>
2024-10-18 18:30:54 -0300

    feat: add overall compatibility for new `price` field in `Toner`
    - optimize logic for `create` and `update` toner
    - use `rust_decimal` for value conversion and numeric formatting
    - implement `Default` for `Toner`

--- src/handlers/supplies/toner.rs
+++ src/handlers/supplies/toner.rs
@@ -82,9 +82,36 @@ pub async fn create_toner(
     State(state): State<Arc<AppState>>,
     Json(request): Json<CreateTonerRequest>,
 ) -> impl IntoResponse {
-    let new_toner = Toner::new(&request.name);
+    // Validations
 
-    // Check duplicate
+    // Name is empty
+    if request.name.is_empty() {
+        error!("Toner name cannot be empty.");
+        return (
+            StatusCode::BAD_REQUEST,
+            Err(Json("Toner name cannot be empty.")),
+        );
+    }
+    // Name too short
+    if request.name.len() < 4 {
+        error!("Toner name is too short.");
+        return (
+            StatusCode::BAD_REQUEST,
+            Err(Json("Toner name is too short.")),
+        );
+    }
+    // Name too long
+    if request.name.len() > 20 {
+        error!("Toner name is too long.");
+        return (
+            StatusCode::BAD_REQUEST,
+            Err(Json("Toner name is too long.")),
+        );
+    }
+
+    let new_toner = Toner::new(&request.name, request.stock, request.price);
+
+    // Check for duplicate toner name
     match sqlx::query(r#"SELECT id FROM toners WHERE name = $1;"#)
         .bind(&new_toner.name)
         .fetch_optional(&state.db)
@@ -95,38 +122,15 @@ pub async fn create_toner(
             (StatusCode::CONFLICT, Err(Json("Toner already exists.")))
         }
         Ok(None) => {
-            // Name is empty
-            if new_toner.name.is_empty() {
-                error!("Toner name cannot be empty.");
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Err(Json("Toner name cannot be empty.")),
-                );
-            }
-
-            // Name too short
-            if new_toner.name.len() < 4 {
-                error!("Toner name is too short.");
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Err(Json("Toner name is too short.")),
-                );
-            }
-
-            // Name too long
-            if new_toner.name.len() > 20 {
-                error!("Toner name is too long.");
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Err(Json("Toner name is too long.")),
-                );
-            }
-
-            match sqlx::query(r#"INSERT INTO toners (id, name) VALUES ($1, $2);"#)
-                .bind(new_toner.id)
-                .bind(&new_toner.name)
-                .execute(&state.db)
-                .await
+            match sqlx::query(
+                r#"INSERT INTO toners (id, name, stock, price) VALUES ($1, $2, $3, $4);"#,
+            )
+            .bind(new_toner.id)
+            .bind(&new_toner.name)
+            .bind(new_toner.stock)
+            .bind(new_toner.price)
+            .execute(&state.db)
+            .await
             {
                 Ok(_) => {
                     info!("Toner created! ID: {}", &new_toner.id);
@@ -153,7 +157,9 @@ pub async fn update_toner(
     Json(request): Json<UpdateTonerRequest>,
 ) -> impl IntoResponse {
     let toner_id = request.id;
-    let new_name = request.name;
+    let new_name = request.name.clone();
+    let new_stock = request.stock;
+    let new_price = request.price;
 
     // ID not found
     match sqlx::query(r#"SELECT id FROM toners WHERE id = $1;"#)
@@ -162,82 +168,108 @@ pub async fn update_toner(
         .await
     {
         Ok(Some(_)) => {
-            // Name is empty
-            if new_name.is_empty() {
-                error!("Toner name cannot be empty.");
-                return (
-                    StatusCode::CONFLICT,
-                    Err(Json("Toner name cannot be empty.")),
-                );
-            }
-
-            // Name too short
-            if new_name.len() < 4 {
-                error!("Toner name is too short.");
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Err(Json("Toner name is too short.")),
-                );
-            }
-
-            // Name too long
-            if new_name.len() > 20 {
-                error!("Toner name is too long.");
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Err(Json("Toner name is too long.")),
-                );
-            }
-
-            // Check duplicate
-            match sqlx::query(r#"SELECT id FROM toners WHERE name = $1 AND id != $2;"#)
-                .bind(&new_name)
-                .bind(toner_id)
-                .fetch_optional(&state.db)
-                .await
-            {
-                Ok(Some(_)) => {
-                    error!("Toner name already exists.");
-                    (
+            if let Some(name) = new_name {
+                if name.is_empty() {
+                    error!("Toner name cannot be empty.");
+                    return (
                         StatusCode::CONFLICT,
-                        Err(Json("Toner name already exists.")),
-                    )
+                        Err(Json("Toner name cannot be empty.")),
+                    );
                 }
-                Ok(None) => {
-                    match sqlx::query(r#"UPDATE toners SET name = $1 WHERE id = $2;"#)
-                        .bind(&new_name)
-                        .bind(toner_id)
-                        .execute(&state.db)
-                        .await
-                    {
-                        Ok(_) => {
-                            info!("Toner updated! ID: {}", &toner_id);
-                            (StatusCode::OK, Ok(Json(toner_id)))
-                        }
-                        Err(e) => {
-                            error!("Error updating toner: {}", e);
-                            (
+
+                if name.len() < 4 {
+                    error!("Toner name is too short.");
+                    return (
+                        StatusCode::BAD_REQUEST,
+                        Err(Json("Toner name is too short.")),
+                    );
+                }
+
+                if name.len() > 20 {
+                    error!("Toner name is too long.");
+                    return (
+                        StatusCode::BAD_REQUEST,
+                        Err(Json("Toner name is too long.")),
+                    );
+                }
+
+                // Check duplicate
+                match sqlx::query(r#"SELECT id FROM toners WHERE name = $1 AND id != $2;"#)
+                    .bind(&name)
+                    .bind(toner_id)
+                    .fetch_optional(&state.db)
+                    .await
+                {
+                    Ok(Some(_)) => {
+                        error!("Toner name already exists.");
+                        return (
+                            StatusCode::CONFLICT,
+                            Err(Json("Toner name already exists.")),
+                        );
+                    }
+                    Ok(None) => {
+                        if let Err(e) = sqlx::query(r#"UPDATE toners SET name = $1 WHERE id = $2;"#)
+                            .bind(&name)
+                            .bind(toner_id)
+                            .execute(&state.db)
+                            .await
+                        {
+                            error!("Error updating toner name: {e}");
+                            return (
                                 StatusCode::INTERNAL_SERVER_ERROR,
-                                Err(Json("Error updating toner.")),
-                            )
+                                Err(Json("Error updating toner name.")),
+                            );
                         }
                     }
-                }
-                Err(e) => {
-                    error!("Error checking for duplicate toner name: {}", e);
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Err(Json("Error checking for duplicated toner name.")),
-                    )
+                    Err(e) => {
+                        error!("Error checking for duplicate toner name: {e}");
+                        return (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            Err(Json("Error checking for duplicated toner name.")),
+                        );
+                    }
                 }
             }
+
+            if let Some(stock) = new_stock {
+                if let Err(e) = sqlx::query(r#"UPDATE toners SET stock = $1 WHERE id = $2;"#)
+                    .bind(stock)
+                    .bind(toner_id)
+                    .execute(&state.db)
+                    .await
+                {
+                    error!("Error updating toner stock: {}", e);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Err(Json("Error updating toner stock.")),
+                    );
+                }
+            }
+
+            if let Some(price) = new_price {
+                if let Err(e) = sqlx::query(r#"UPDATE toners SET price = $1 WHERE id = $2;"#)
+                    .bind(price)
+                    .bind(toner_id)
+                    .execute(&state.db)
+                    .await
+                {
+                    error!("Error updating toner price: {e}");
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Err(Json("Error updating toner price.")),
+                    );
+                }
+            }
+
+            info!("Toner updated! ID: {}", &toner_id);
+            (StatusCode::OK, Ok(Json(toner_id)))
         }
         Ok(None) => {
             error!("Toner ID not found.");
             (StatusCode::NOT_FOUND, Err(Json("Toner ID not found.")))
         }
         Err(e) => {
-            error!("Error fetching toner by ID: {}", e);
+            error!("Error fetching toner by ID: {e}");
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 Err(Json("Error fetching toner by ID")),
--- src/models/supplies/toner.rs
+++ src/models/supplies/toner.rs
@@ -1,3 +1,4 @@
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use uuid::Uuid;
@@ -6,15 +7,29 @@ use uuid::Uuid;
 pub struct Toner {
     pub id: Uuid,
     pub name: String,
-    pub stock: i32,
+    pub stock: Option<i32>,
+    #[serde(with = "rust_decimal::serde::float_option")]
+    pub price: Option<Decimal>,
+}
+
+impl Default for Toner {
+    fn default() -> Self {
+        Toner {
+            id: Uuid::new_v4(),
+            name: String::from("Unknown"),
+            stock: None,
+            price: None,
+        }
+    }
 }
 
 impl Toner {
-    pub fn new(name: &str) -> Self {
+    pub fn new(name: &str, stock: Option<i32>, price: Option<Decimal>) -> Self {
         Toner {
             id: Uuid::new_v4(),
             name: String::from(name),
-            stock: 0,
+            stock,
+            price,
         }
     }
 }
@@ -22,10 +37,14 @@ impl Toner {
 #[derive(Deserialize, Serialize)]
 pub struct CreateTonerRequest {
     pub name: String,
+    pub stock: Option<i32>,
+    pub price: Option<Decimal>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct UpdateTonerRequest {
     pub id: Uuid,
-    pub name: String,
+    pub name: Option<String>,
+    pub stock: Option<i32>,
+    pub price: Option<Decimal>,
 }
