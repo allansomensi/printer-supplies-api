1b202095498b17eef13b4d96f106b89909f58326
Allan Somensi <allansomensidev@gmail.com>
2024-10-28 14:32:44 -0300

    refactor(config): separate `logger` and `CORS` logic into individual files

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,9 +1,7 @@
-use chrono::{DateTime, FixedOffset, Utc};
 use dotenvy::Error as DotenvError;
-use std::fmt;
-use tower_http::cors::{Any, CorsLayer};
-use tracing_subscriber::fmt::format::Writer;
-use tracing_subscriber::fmt::time::FormatTime;
+
+pub mod cors;
+pub mod logger;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -23,38 +21,5 @@ impl Config {
     pub fn init() -> Result<(), ConfigError> {
         dotenvy::dotenv()?;
         Ok(())
-    }
-
-    pub fn cors() -> CorsLayer {
-        let origins = [
-            "http://127.0.0.1:3000".parse().unwrap(),
-            "http://localhost:3000".parse().unwrap(),
-        ];
-
-        CorsLayer::new()
-            .allow_origin(origins)
-            .allow_methods(Any)
-            .allow_headers(Any)
-    }
-
-    pub fn logger_init() {
-        struct UtcFormattedTime;
-
-        impl FormatTime for UtcFormattedTime {
-            fn format_time(&self, writer: &mut Writer<'_>) -> fmt::Result {
-                let brasilia_offset = FixedOffset::west_opt(3 * 3600).unwrap();
-                let now: DateTime<FixedOffset> = Utc::now().with_timezone(&brasilia_offset);
-                let formatted_time = now.format("%d/%m/%Y %H:%M:%S").to_string();
-                write!(writer, "{}", formatted_time)
-            }
-        }
-
-        tracing_subscriber::fmt()
-            .pretty()
-            .with_timer(UtcFormattedTime)
-            .with_file(false)
-            .with_line_number(false)
-            .with_target(false)
-            .init();
     }
 }
