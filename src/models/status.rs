f2313280568c4f2605298950a19db2dc8c85a069
Allan Somensi <allansomensidev@gmail.com>
2024-10-29 17:49:30 -0300

    docs(status): update documentation for status endpoint

--- src/models/status.rs
+++ src/models/status.rs
@@ -1,21 +1,22 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 
 type Version = String;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, ToSchema)]
 pub struct Database {
     pub version: Version,
     pub max_connections: u16,
     pub opened_connections: u16,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, ToSchema)]
 pub struct Dependencies {
     pub database: Database,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, ToSchema)]
 pub struct Status {
     pub updated_at: DateTime<Utc>,
     pub dependencies: Dependencies,
