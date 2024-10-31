cd76bf45abb24aec414d9b8ec2a1f951a1937061
Allan Somensi <allansomensidev@gmail.com>
2024-10-31 19:50:33 -0300

    feat(drums): integrate custom error handling and validations in `drum` handlers

--- src/validations/uniqueness.rs
+++ src/validations/uniqueness.rs
@@ -21,3 +21,22 @@ pub async fn is_toner_unique(state: Arc<AppState>, toner_name: String) -> Result
         Ok(())
     }
 }
+
+pub async fn is_drum_unique(state: Arc<AppState>, drum_name: String) -> Result<(), ApiError> {
+    let exists = sqlx::query(r#"SELECT id FROM drums WHERE name = $1;"#)
+        .bind(&drum_name)
+        .fetch_optional(&state.db)
+        .await
+        .map_err(|e| {
+            error!("Error checking for existing drum: {e}");
+            ApiError::DatabaseError(e)
+        })?
+        .is_some();
+
+    if exists {
+        error!("Drum '{}' already exists.", &drum_name);
+        Err(ApiError::AlreadyExists)
+    } else {
+        Ok(())
+    }
+}
